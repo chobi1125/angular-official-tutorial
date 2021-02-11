5ab8dcf36de93952feaa0000cea99423e93a7967
masaru sato <>
2021-02-11 20:36:43 +0900

    MessageServiceの導入

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -2,13 +2,14 @@ import { Injectable } from '@angular/core';
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 import { Observable, of } from 'rxjs';
+import { MessageService } from './message.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-  constructor() { }
+  constructor(private messageService: MessageService) { }
 
   // 同期処理
   // getHeroes(): Hero[] {
@@ -18,6 +19,7 @@ export class HeroService {
   // 非同期処理
   getHeroes(): Observable<Hero[]> {
     // of(HEROES)はモックヒーローの配列を出力するObservable<Hero[]>を返す
+    this.messageService.add('HeroService: fetched heroes');
     return of(HEROES);
   }
 }
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 @Component({
   selector: 'app-heroes',
   template: `
@@ -22,7 +23,7 @@ export class HeroesComponent implements OnInit {
   heroes: Hero[];
   selectedHero: Hero;
 
-  constructor(private heroService: HeroService) {}
+  constructor(private heroService: HeroService, private messageService: MessageService) { }
 
   ngOnInit() {
     this.getHeroes();
@@ -30,6 +31,7 @@ export class HeroesComponent implements OnInit {
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
 
   // 同期処理
