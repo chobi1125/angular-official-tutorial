eabfed30a3bb17cfac2f0f930fd306017dd79fa6
masaru sato <>
2021-02-11 21:27:54 +0900

    HttpClientModuleとインメモリAPIの導入

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -3,29 +3,72 @@ import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 import { Observable, of } from 'rxjs';
 import { MessageService } from './message.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError, map, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-  constructor(private messageService: MessageService) { }
+  private heroesUrl = 'api/heroes';  // Web APIのURL
+
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService) { }
 
   // 同期処理
   // getHeroes(): Hero[] {
   //   return HEROES;
   // }
+
+  /** HeroServiceのメッセージをMessageServiceを使って記録 */
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
+  }
   
   // 非同期処理
+  // getHeroes(): Observable<Hero[]> {
+  //   // of(HEROES)はモックヒーローの配列を出力するObservable<Hero[]>を返す
+  //   this.messageService.add('HeroService: fetched heroes');
+  //   return of(HEROES);
+  // }
+
+  /** サーバーからヒーローを取得する */
   getHeroes(): Observable<Hero[]> {
-    // of(HEROES)はモックヒーローの配列を出力するObservable<Hero[]>を返す
-    this.messageService.add('HeroService: fetched heroes');
-    return of(HEROES);
+    return this.http.get<Hero[]>(this.heroesUrl)
+      .pipe(
+        tap(heroes => this.log('fetched heroes')),
+        catchError(this.handleError<Hero[]>('getHeroes', []))
+      );
   }
 
+  /** IDによりヒーローを取得する。見つからなかった場合は404を返却する。 */
   getHero(id: number): Observable<Hero> {
-    // TODO: send the message _after_ fetching the hero
-    this.messageService.add(`HeroService: fetched hero id=${id}`);
-    return of(HEROES.find(hero => hero.id === id));
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.get<Hero>(url).pipe(
+      tap(_ => this.log(`fetched hero id=${id}`)),
+      catchError(this.handleError<Hero>(`getHero id=${id}`))
+    );
+  }
+
+  /**
+   * 失敗したHttp操作を処理します。
+   * アプリを持続させます。
+   * @param operation - 失敗した操作の名前
+   * @param result - observableな結果として返す任意の値
+   */
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+
+      // TODO: リモート上のロギング基盤にエラーを送信する
+      console.error(error); // かわりにconsoleに出力
+
+      // TODO: ユーザーへの開示のためにエラーの変換処理を改善する
+      this.log(`${operation} failed: ${error.message}`);
+
+      // 空の結果を返して、アプリを持続可能にする
+      return of(result as T);
+    };
   }
 }
