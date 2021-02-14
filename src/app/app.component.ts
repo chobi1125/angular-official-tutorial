6e3eee211cbf345ffbcb0332013932c2fcabc2c3
masaru sato <>
2021-02-14 20:33:41 +0900

    簡易的なコンポーネント作成

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <h1>{{ title }}</h1>
+    <p [class]="styleClass">{{ today() }}</p>
+  `,
+  styles: [`
+    h1 {
+      color: #369;
+      font-size: 150%;
+    }
+    .red {
+      color: white;
+      background-color: red;
+    }
+  `]
 })
-export class AppComponent {
-  title = 'Tour of Heroes';
+export class AppComponent implements OnInit {
+  title: string;
+  now: Date;
+  styleClass: string;
+
+  constructor() {
+    setInterval(() => {
+      this.now = new Date()
+      // this.styleClass = this.styleClass == "red" ? "" : "red";
+    }, 1000)
+  }
+
+  ngOnInit(){
+    this.title = "Tour of Heroes";
+    this.styleClass = "red"
+  }
+
+  today() {
+    return this.now.toLocaleString(); 
+    // toLocaleString()はいい具合に表示を整形してくれる。
+    // ない場合：Sun Feb 14 2021 20:08:28 GMT+0900 (日本標準時)
+    // ある場合：2021/2/14 20:08:41
+  }
 }
